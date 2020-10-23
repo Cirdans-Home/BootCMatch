afda28af9cf794dbc7e22beb98a65a88a8322f95
Cirdans-Home <f.durastante@na.iac.cnr.it>
2020-10-23 15:18:41 +0200

    Compile and Install with CMake

---
+++ cmake/FindHSL.cmake
@@ -0,0 +1,20 @@
+# Looking for HSL MC64 Library
+set(HSL_DIR /opt/hsl
+						CACHE PATH "/opt/hsl")
+
+find_library(HSL_LIBRARIES libhsl_mc64.a
+						 PATHS ${HSL_DIR}/lib REQUIRED)
+
+set(HSL_INCDIR ${HSL_DIR}/include)
+set(HSL_LIBDIR ${HSL_DIR}/lib)
+set(HSL_LIBS -lhsl_mc64 -L${HSL_LIBDIR})
+set(HSL_FLAGS HAVE_HSL)
+
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+	"HSL"
+	FOUND_VAR HSL_FOUND
+	REQUIRED_VARS HSL_INCDIR HSL_LIBDIR HSL_LIBS HSL_FLAGS
+	FAIL_MESSAGE "Couldn't find HSL"
+	)
---
+++ cmake/FindSPRAL.cmake
@@ -0,0 +1,19 @@
+# Looking for SPRAL Library
+set(SPRAL_DIR /opt/hsl
+						CACHE PATH "/opt/hsl")
+
+find_library(SPRAL_LIBRARIES libspral.a
+						 PATHS ${SPRAL_DIR}/lib REQUIRED)
+
+set(SPRAL_INCDIR ${SPRAL_DIR}/include)
+set(SPRAL_LIBDIR ${SPRAL_DIR}/lib)
+set(SPRAL_LIBS -lspral -L${SPRAL_LIBDIR})
+set(SPRAL_FLAGS HAVE_SPRAL)
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(
+	"SPRAL"
+	FOUND_VAR SPRAL_FOUND
+	REQUIRED_VARS SPRAL_INCDIR SPRAL_LIBDIR SPRAL_LIBS SPRAL_FLAGS
+	FAIL_MESSAGE "Couldn't find SPRAL"
+	)
